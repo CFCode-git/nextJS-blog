4c5d75730229c619b1401a37eeab8b6f8a5ecbca
CFCode-git <435184766@qq.com>
2020-12-21 16:44:06 +0800

    安装lodash, user序列化时隐藏敏感和不必要的信息

--- pages/api/v1/users.tsx
+++ pages/api/v1/users.tsx
@@ -5,13 +5,13 @@ import md5 from 'md5';
 
 const Users: NextApiHandler = async (req, res) => {
   const {username, password, passwordConfirmation} = req.body;
-  const connection = await getDatabaseConnection(); // 第一次连接不能用 get
+  // 连接数据库 创建用户
+  const connection = await getDatabaseConnection();
 
   const user = new User();
   user.username = username.trim();
   user.password = password;
   user.passwordConfirmation = passwordConfirmation;
-  user.passwordDigest = md5(password);
   // 表单验证
   await user.validate();
   res.setHeader('Content-Type', 'application/json; charset=utf8');
@@ -19,7 +19,6 @@ const Users: NextApiHandler = async (req, res) => {
     res.statusCode = 422;
     res.write(JSON.stringify(user.errors));
   } else {
-    // 连接数据库 创建用户
     await connection.manager.save(user);
     res.statusCode = 200;
     res.write(JSON.stringify(user));
