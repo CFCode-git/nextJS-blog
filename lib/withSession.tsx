96919fdadb4af8aa578e3fe91656895bebe09133
CFCode-git <435184766@qq.com>
2020-12-23 09:26:48 +0800

    使用 .env.local 设置环境变量

--- lib/withSession.tsx
+++ lib/withSession.tsx
@@ -4,11 +4,8 @@ import {NextApiHandler} from 'next';
 
 export function withSession(handler: NextApiHandler) {
   return withIronSession(handler, {
-    // password: process.env.SECRET_COOKIE_PASSWORD, 对 cookie 加密的密钥
-    password: '7df28029-b28d-4e59-bdac-fe8e84cabb3c', // 方便测试, 先这样写
+    password: process.env.SECRET,
     cookieName: 'blog',
-    cookieOptions: {
-      secure: false
-    }
+    cookieOptions: {secure: false}
   });
 }
