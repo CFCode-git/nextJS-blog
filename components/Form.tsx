8d241a818277c79b0831fd35fedba9a52919d378
CFCode-git <435184766@qq.com>
2020-12-23 15:48:41 +0800

    posts/new 页面初步完成

--- components/Form.tsx
+++ components/Form.tsx
@@ -4,9 +4,9 @@ type Props = {
   onSubmit: FormEventHandler;
   fields: {
     label: string,
-    type: 'text' | 'password',
+    type: 'text' | 'password' | 'textarea',
     value: string | number,
-    onChange: ChangeEventHandler<HTMLInputElement>,
+    onChange: ChangeEventHandler<HTMLInputElement | HTMLTextAreaElement>,
     errors: string[]
   }[];
   buttons:ReactChild;
@@ -17,9 +17,10 @@ const Form: React.FunctionComponent<Props> = (props) => {
     <form onSubmit={props.onSubmit}>
       {props.fields.map(field => <div>
         <label>{field.label}
-          <input type={field.type} value={field.value}
-                 onChange={field.onChange}
-          />
+          {field.type === 'textarea' ?
+            <textarea onChange={field.onChange}>{field.value}</textarea> :
+            <input type={field.type} value={field.value} onChange={field.onChange}/>
+          }
         </label>
         {field.errors?.length > 0 && <div>
           {field.errors.join(',')}
--- pages/posts/new.tsx
+++ pages/posts/new.tsx
@@ -1,14 +1,58 @@
 import {NextPage} from 'next';
-import React from 'react';
+import React, {useCallback, useState} from 'react';
+import Form from '../../components/Form';
+import axios, {AxiosResponse} from 'axios';
 
 const PostsNew: NextPage = () => {
+  const [formData, setFormData] = useState({
+    title: '',
+    content: ''
+  });
+
+  const [errors, setErrors] = useState({
+    title: [],
+    content: []
+  });
+  const onChange = useCallback((key, value) => {
+    setFormData({...formData, [key]: value});
+  }, [formData]);
+
+  const onSubmit = useCallback((e) => {
+    e.preventDefault();
+    axios.post(`/api/v1/posts`, formData)
+      .then(() => {
+        window.alert(`提交成功`);
+      }, (error) => {
+        if (error.response) {
+          const response: AxiosResponse = error.response;
+          if (response.status === 422) {
+            setErrors(response.data);
+          }
+        }
+      });
+  }, [formData]);
+
   return (
     <div>
-      <form>
-        <div>hi</div>
-      </form>
+      <Form fields={[
+        {
+          label: '标题',
+          type: 'text', value: '',
+          onChange: e => onChange('title', e.target.value),
+          errors: errors.title,
+        },
+        {
+          label: '内容',
+          type: 'textarea', value: '',
+          onChange: e => onChange('content', e.target.value),
+          errors: errors.content,
+        },
+      ]} onSubmit={onSubmit} buttons={<>
+        <button type='submit'>提交</button>
+      </>}/>
     </div>
-  );
+  )
+    ;
 };
 export default PostsNew;
 
