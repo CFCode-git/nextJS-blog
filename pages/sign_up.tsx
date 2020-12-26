f0572eac92acc7447dc0762dd3a8d1bb74fc5932
CFCode-git <435184766@qq.com>
2020-12-26 20:57:53 +0800

    成功使用 useForm

--- pages/sign_up.tsx
+++ pages/sign_up.tsx
@@ -4,21 +4,19 @@ import axios, {AxiosResponse} from 'axios';
 import {useForm} from '../Hooks/useForm';
 
 const SignUp: NextPage = () => {
-
-  const initFormData = {username: '', password: '', passwordConfirmation: '',};
   const {form, setErrors} = useForm({
-    initFormData,
+    initFormData:{username: '', password: '', passwordConfirmation: '',},
     fields: [
       {label: '用户名', type: 'text', key: 'username'},
       {label: '密码', type: 'password', key: 'password'},
       {label: '确认密码', type: 'password', key: 'passwordConfirmation',},
     ],
-    buttons: <button type="submit">登录</button>,
+    buttons: <button type="submit">注册</button>,
     submit: {
       request(formData){
         return axios.post(`/api/v1/users`, formData)
       },
-      message: '提交成功'
+      message: '注册成功'
     }
   });
 
