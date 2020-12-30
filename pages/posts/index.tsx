d8a9c8ef5f48b757bedcc5b50df5338e04755344
CFCode-git <435184766@qq.com>
2020-12-30 21:58:13 +0800

    使用分页器 usePager.tsx

--- pages/posts/index.tsx
+++ pages/posts/index.tsx
@@ -5,6 +5,7 @@ import {getDatabaseConnection} from 'lib/getDatabaseConnection';
 import {Post} from 'src/entity/Post';
 import Link from 'next/link';
 import qs from 'querystring';
+import {usePager} from '../../Hooks/usePager';
 
 
 type Props = {
@@ -14,8 +15,10 @@ type Props = {
   page: number,
   totalPage: number
 }
+
 const PostsIndex: NextPage<Props> = (props) => {
-  const {posts} = props;
+  const {posts, count, page, totalPage} = props;
+  const {pager} = usePager({page, totalPage});
   return (
     <div>
       <h1>文章列表</h1>
@@ -26,19 +29,7 @@ const PostsIndex: NextPage<Props> = (props) => {
         </div>
       )}
       <footer>
-        共 {props.count} 篇文章, 当前是第 {props.page} / {props.totalPage} 页
-        {props.page !== 1 &&
-        <Link href={`?page=${props.page - 1}`}>
-          <a>上一页</a>
-        </Link>
-        }
-        |
-        {}
-        {props.page < props.totalPage &&
-        <Link href={`?page=${props.page + 1}`}>
-          <a>下一页</a>
-        </Link>
-        }
+        {pager}
       </footer>
     </div>
   );
@@ -52,7 +43,7 @@ export const getServerSideProps: GetServerSideProps = async (context) => {
   const query = qs.parse(search); // 得到query对象 {page:'1'}
   const page = parseInt(query.page?.toString()) || 1;
   const connection = await getDatabaseConnection();
-  const perPage = 3;
+  const perPage = 1;
   const [posts, count] = await connection.manager.findAndCount(Post, {skip: (page - 1) * perPage, take: perPage});
 
   return {
