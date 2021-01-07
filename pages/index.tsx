89e84f47ed11c7bd37bf9dcecfebf3b4ec8f1aa0
CFCode-git <435184766@qq.com>
2021-01-07 12:02:49 +0800

    部署出现问题,再次添加首页和logo;安装sass和classnames

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,4 +1,28 @@
 import PostsIndex, {getServerSideProps} from './posts';
+import {NextPage} from 'next';
+import React from 'react';
+import Link from 'next/link';
 
-export default PostsIndex;
-export {getServerSideProps};
+const Home:NextPage = ()=>{
+  return (
+    <>
+      <div className='cover'>
+        <img src="/logo.png" alt=""/>
+        <h1>呆尬猴的个人博客 - 测试网站</h1>
+        <p>瞎写一下,以后加内容</p>
+        <p><Link href="/posts"><a>文章列表</a></Link></p>
+      </div>
+      <style jsx>{`
+        .cover{
+          height: 100vh;
+          display: flex;
+          justify-content: center;
+          align-items: center;
+          flex-direction:column;
+        }
+      `}</style>
+    </>
+  )
+};
+export default Home;
+
