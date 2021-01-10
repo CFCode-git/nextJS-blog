7099af81f6117127aa9600c37e3bc9bfb6031800
CFCode-git <435184766@qq.com>
2021-01-10 22:34:05 +0800

    update deploy.sh

--- bin/deploy.sh
+++ bin/deploy.sh
@@ -1,1 +1,8 @@
-echo Hello > /tmp/chau
+docker start 2c5 &&
+cd /home/blog/app/ &&
+git pull &&
+yarn install --production=false &&
+yarn build &&
+docker build -t chau/node-web-app . &&
+docker run --name app --network=host -p 3000:3000 -d chau/node-web-app
+echo 'OK!'
