c73c16bb64bd7643d29986001933012eee1b46d8
CFCode-git <435184766@qq.com>
2021-01-11 00:01:54 +0800

    update deploy.sh

--- bin/deploy.sh
+++ bin/deploy.sh
@@ -2,8 +2,8 @@ docker start 2c5 &&
 cd /home/blog/app/ &&
 git pull &&
 yarn install --production=false &&
-yarn build &&
 git apply migrate.patch &&
+yarn build &&
 yarn m:run &&
 git reset --hard HEAD &&
 docker build -t chau/node-web-app . &&
