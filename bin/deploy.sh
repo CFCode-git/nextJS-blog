01b6dc57b695ce3e68d35ee0c4f62f856f1c5b7a
CFCode-git <435184766@qq.com>
2021-01-10 23:58:49 +0800

    update deploy.sh

--- bin/deploy.sh
+++ bin/deploy.sh
@@ -3,6 +3,9 @@ cd /home/blog/app/ &&
 git pull &&
 yarn install --production=false &&
 yarn build &&
+git apply migrate.patch &&
+yarn m:run &&
+git reset --hard HEAD &&
 docker build -t chau/node-web-app . &&
 docker kill app && docker rm app &&
 docker run --name app --network=host -p 3000:3000 -d chau/node-web-app
